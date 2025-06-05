94a56e457e7bc4136de50386e00788ae8ca832a8
Thalia Nero <chrisvn00@gmail.com>
2025-06-05 17:11:54 -0500

    Remove drink states from tall glass.

--- src/main/java/systems/thedawn/espresso/block/TallGlassBlock.java
+++ src/main/java/systems/thedawn/espresso/block/TallGlassBlock.java
@@ -5,27 +5,15 @@ import systems.thedawn.espresso.EspressoItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class TallGlassBlock extends AbstractDrinkBlock {
-    public static final Property<Boolean> HAS_MILK = BooleanProperty.create("has_milk");
-    public static final Property<Boolean> HAS_BUBBLES = BooleanProperty.create("has_bubbles");
 
     public TallGlassBlock(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder);
-        builder.add(HAS_MILK, HAS_BUBBLES);
     }
 
     @Override
